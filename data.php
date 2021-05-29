e02172c425f6ef26a7cc4939fb0e43fdd4cdbe23
Alejandro M <alejandromatos1302@gmail.com>
2021-05-28 23:57:31 -0400

    cleaning
    cleaning

--- data.php
+++ data.php
@@ -1,10 +1,8 @@
 <?php
-
 function getData(){
     $csv = array_map('str_getcsv', file('https://docs.google.com/spreadsheets/d/e/2PACX-1vSWya2xLTMvCoMjrQVKKAvhd4uIj1YxH0jUW4m-fKgWRmhHuPzq-4xRAaC8hMvHdTFlpknL3Bp6HWH6/pub?gid=0&single=true&output=csv'));
     return $csv;
 }
-
 ?>
 
 
